b41bab5ec21d38715de7358a80e2b439936c93a0
Youenn PENNARUN <youenn.pennarun@gmail.com>
2015-10-23 21:19:04 +0200

    db

--- server/config.js
+++ server/config.js
@@ -1,4 +1,29 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
 module.exports = {
 	"path": "alarms",
-	"require": [{module: "homyPi-server-music", version: "0.1"}]
+	"require": [{module: "homyPi-server-music", version: "0.1"}],
+	"setSchemaDescriptions": function(schemaDescriptions) {
+		schemaDescriptions.alarm = {
+			user: Schema.Types.Mixed,
+			hours: { type: Number, min: 0, max: 23 },
+			minutes: { type: Number, min: 0, max: 59 },
+			enable: Boolean,
+			repeat: Schema.Types.Mixed,
+			history: [{
+				execution_date: Date,
+				executed_songs: [{
+					source: String,
+					name: String,
+					artist: String,
+					uri: String,
+					similarTo: Schema.Types.Mixed,
+					tempo: String
+				}]
+			}]
+		};
+	},
+	"setSchemas": function(schemaDescriptions) {
+		mongoose.model('Alarm', new Schema(schemaDescriptions.alarm));	
+	}
 };
