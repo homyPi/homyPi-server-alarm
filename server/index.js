3c628be70adaa416f9c86220de0e87f9d83c36d3
Youenn PENNARUN <youenn.pennarun@gmail.com>
2015-11-04 00:23:13 +0100

    bug fix

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
-var routes = require("./alarmRoutes");
+var routes = require("./modules/alarmRoutes");
+var Link = require("./Link");
 module.exports = {
 	link: function() {},
 	setSocket: function(socket) {
-		require("./alarmSocket")(socket);
+		require("./modules/alarmSocket")(socket);
 	},
 	routes: function(app, router) {
 		routes(router);
 		return router;
 	},
-	config: require("./config")
+	config: require("./config"),
+	link: function(moduleManager, Raspberry, MongooseModels, UserMiddleware) {
+		Link.Raspberry = Raspberry;
+		Link.MongooseModels = MongooseModels;
+		Link.User = {
+			middleware: UserMiddleware
+		}
+	}
 }
